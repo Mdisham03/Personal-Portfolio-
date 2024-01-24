a0a65bf453455b433dbb3e6320ac887a6e600e4c
Mdisham03 <md.isham03@gmail.com>
2024-01-24 22:20:50 +0530

    another H1 added

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <h1>Mohamed Isham</h1>
+        <h1>2001</h1>
+
       </header>
     </div>
   );
