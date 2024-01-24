7997a49806450e183c04f636cafb7e43573a9bd1
Mdisham03 <md.isham03@gmail.com>
2024-01-24 22:27:02 +0530

    footer.  added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,18 +5,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Mohamed </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="footer">
+      <p>
+        Made with ❤ Md.Isham
+      </p>
+    </div>
       </header>
     </div>
   );
